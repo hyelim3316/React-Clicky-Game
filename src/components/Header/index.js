038d90277c9c1af0120488845ff88cee76c1267f
hyelim3316@gmail.com <hyelim3316@gmail.com>
2019-07-02 14:33:20 -0400

    css updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Header() {
   return(
-  <div className="jumbotron jumbotron-fluid">
-  <div className="container">
+  <div className="jumbotron d-flex align-items-center">
+  <div className="container vertical-center">
     <h1 className="display-4">Pokemon Clicky Game!</h1>
       <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
   </div>
